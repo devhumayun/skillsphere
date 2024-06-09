ec400c9a32d20fcbc470c67331ccda3155b9375e
devhumayun <humayunkabir9408@gmail.com>
2024-06-09 14:24:08 +0600

    logo remove from site footer

--- components/SiteFooter.jsx
+++ components/SiteFooter.jsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { Logo } from "./Logo";
+
 
 
 export function SiteFooter({ className }) {
@@ -7,7 +7,6 @@ export function SiteFooter({ className }) {
         <footer className={cn(className)}>
             <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-                    <Logo />
                     <p className="text-center text-sm leading-loose md:text-left">
                         Built by{" "}
                         <a
