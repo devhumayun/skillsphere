88305115a9d90c2f91a3a0018e1def0e038192d2
devhumayun <humayunkabir9408@gmail.com>
2024-06-12 23:12:58 +0600

    course overview data added

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         hostname: "res.cloudinary.com",
       },
       {
-        hostname: "example.com",
+        hostname: "avatars.githubusercontent.com",
       },
     ],
   },
