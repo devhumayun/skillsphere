af0165e98a49baac24d388df0416fbcc5b82780e
devhumayun <humayunkabir9408@gmail.com>
2024-06-21 17:56:02 +0600

    refresh token access token feature added for google auth login

--- auth.js
+++ auth.js
@@ -1,9 +1,51 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { authConfig } from "./auth.Config";
 import { User } from "./models/user-model";
+import mongoClientPromise from "./services/mongoClientPromise";
+
+const refreshAccessToken = async (token) => {
+  try {
+    const url =
+      "https://oauth2.googleapis.com/token" +
+      new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        grant_type: "refresh_token",
+        refresh_token: token?.refreshToken,
+      });
+
+    const response = fetch(url, {
+      headers: {
+        "Content=Type": "appliation/x-www-form-urlencoded",
+      },
+      method: "POST",
+    });
+
+    const refreshTokens = await response.json();
+
+    if (!response.ok) {
+      return refreshTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshTokens?.access_token,
+      accessTokenExpireIn: Date.now() + refreshTokens?.expires_in * 1000,
+      refreshToken: refreshTokens?.refresh_token,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+};
 
 export const {
   handlers: { GET, POST },
@@ -11,6 +53,9 @@ export const {
   auth,
   signOut,
 } = NextAuth({
+  adapter: MongoDBAdapter(mongoClientPromise, {
+    databaseName: process.env.DATABASE_NAME,
+  }),
   ...authConfig,
   providers: [
     CredentialsProvider({
@@ -41,7 +86,40 @@ export const {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        return {
+          user,
+          accessToken: account?.access_token,
+          accessTokenExpireIn: Date.now() + account?.expires_in * 1000,
+          refreshToken: account?.refresh_token,
+        };
+      }
+
+      if (Date.now() < token?.accessTokenExpireIn) {
+        return token;
+      }
+
+      return refreshAccessToken(token);
+    },
+    async session({ token, session }) {
+      session.user = token?.user;
+      (session.accessToken = token?.access_token),
+        (session.error = token?.error);
+
+      return session;
+    },
+  },
+  trustHost: true,
+  trustHostedDomain: true,
 });
