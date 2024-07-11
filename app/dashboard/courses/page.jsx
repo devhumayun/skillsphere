4f074855901b95eca14d00ad62f11c005522228c
devhumayun <humayunkabir9408@gmail.com>
2024-07-11 15:03:16 +0600

    make the dashboard/course forcefully dynamic

--- app/dashboard/courses/page.jsx
+++ app/dashboard/courses/page.jsx
@@ -2,6 +2,8 @@ import { COURSES_DATA, instructorDashboardData } from "@/lib/dashboardHelper";
 import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
 
+export const dynamic = 'force-dynamic';
+
 const CoursesPage = async () => {
 
   const courses = await instructorDashboardData(COURSES_DATA)
